2965988aa21c135bb196f794d89fdfb1459f4a25
Huy Nguyen <huykgit98@gmail.com>
2025-06-30 18:28:29 +0700

    Initial landing page for Vega AI Money Tracker

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,51 +11,50 @@ import Review from '@/components/Review';
 export default function Home() {
   return (
     <>
-      <main className={styles.main}>        
+      <main className={styles.main}>
         <NodeOverlay />
-        < Hero 
-          title="Private Communities, Powered By Voice"
-          subtitle="Roads Audio helps you build deeper connections through flexible, on-your-time voice conversations that fit your busy life."
+        <Hero
+          title="Vega – AI Money Tracker"
+          subtitle="Track every expense in seconds, let AI auto-categorize for you, and see your budget insights in real time."
         />
 
-        <div className='text-center'>
+        <div className="text-center">
           <h2 className="h2-lg my-lg my-lg">Watch The Demo</h2>
-          
-          <iframe 
+
+          <iframe
             className="youtube-video"
-            src="https://www.youtube.com/embed/w_H7ooZYnAY" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            loading='eager'
-            >
-          </iframe>
+            src="https://www.youtube.com/embed/w_H7ooZYnAY"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            loading="eager"
+          ></iframe>
         </div>
 
-         <div id={styles.section_reviews}>
+        <div id={styles.section_reviews}>
           <h2 className="h2-lg my-lg text-center">What People Are Saying</h2>
           <div className="container">
             <div className="row">
-              <Review 
+              <Review
                 body="Roads allows you to use your voice to stay connected with the world. The app is designed beautifully and is really intuitive."
                 name="Nolanmakatche"
               />
-              <Review 
+              <Review
                 body="I first started using this app with a friend and it's a lot of fun. We use this app now instead of texting or voicemails since we are both traveling and it makes keeping up with each other a lot easier."
                 name="Sethconner10"
               />
-              <Review 
+              <Review
                 body="One of the coolest apps I've used in a long time. A great way to communicate with family and friends."
                 name="neenu17"
               />
-              <Review 
+              <Review
                 body="A game changer, the interactive audio comments bring conversations to life. It's been super fun chatting with my friends from all around the world on Roads."
                 name="makk1ntosh"
               />
-              <Review 
+              <Review
                 body="I've tried a lot of chat apps and didn't think Roads would be any different. Glad I gave it a chance because it's much better than I expected!"
                 name="LFO16"
                 className="d-none d-md-block"
               />
-              <Review 
+              <Review
                 body="I was not expecting to use this app as much as I have. It really pulls you in once you start using it!"
                 name="Jengla11"
                 className="d-none d-md-block"
@@ -64,9 +63,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='text-center py-5 mt-5 bg-graident'>
+        <div className="text-center py-5 mt-5 bg-graident">
           <Link href="https://www.f6s.com/roads-audio" target="_blank">
-            <Image 
+            <Image
               src="/images/f6s.png"
               alt="Top Telecom startups in Virginia"
               height={60}
@@ -74,16 +73,18 @@ export default function Home() {
             />
           </Link>
         </div>
- 
+
         <div id={styles.section_features}>
           <h2 className="h2-lg my-lg text-center">How Roads Is Unique</h2>
 
           <div className="container">
             <div className="row justify-content-md-center">
-              <div className={`${styles.backsplash_container} col-md-6 col-lg-4 text-center`} >
+              <div
+                className={`${styles.backsplash_container} col-md-6 col-lg-4 text-center`}
+              >
                 <div className={`${styles.backsplash} mx-auto`}></div>
-                <Image 
-                  src="/images/phones/device_15_player_simple2.png" 
+                <Image
+                  src="/images/phones/device_15_player_simple2.png"
                   alt="Screenshot of Roads app showing the main player with several comments."
                   height={400}
                   width={200}
@@ -91,36 +92,44 @@ export default function Home() {
                 />
               </div>
 
-              <div className='col-md-6 col-lg-4 offset-lg-1 my-auto text-center text-md-start'>
+              <div className="col-md-6 col-lg-4 offset-lg-1 my-auto text-center text-md-start">
                 <h3>Interactive Conversations</h3>
-                  <p>
-                    Comment on audio with your voice, creating a completely new layered communication experience.
-                  </p>
+                <p>
+                  Comment on audio with your voice, creating a completely new
+                  layered communication experience.
+                </p>
               </div>
             </div>
-            
+
             <div className="row my-5 justify-content-md-center">
-              <div className={`${styles.backsplash_container} col-md-6 col-lg-4 offset-lg-1 text-center`} >
+              <div
+                className={`${styles.backsplash_container} col-md-6 col-lg-4 offset-lg-1 text-center`}
+              >
                 <div className={`${styles.backsplash} mx-auto`}></div>
-                <Image 
-                  src="/images/phones/device_15_player_nested.png" 
+                <Image
+                  src="/images/phones/device_15_player_nested.png"
                   alt="Screenshot of Roads app showing the home page where new comments left by other users can be listened to."
                   height={400}
                   width={200}
                   className={`${styles.backsplash_image} phone_shadow`}
                 />
               </div>
-               <div className='col-md-6 col-lg-4 my-auto order-md-first text-center text-md-start'>
+              <div className="col-md-6 col-lg-4 my-auto order-md-first text-center text-md-start">
                 <h3>Always Have Context</h3>
-                <p>When listening to a comment know exactly what's being referenced, and easily navigate the asynchronous conversation.</p>
+                <p>
+                  When listening to a comment know exactly what's being
+                  referenced, and easily navigate the asynchronous conversation.
+                </p>
               </div>
             </div>
 
             <div className="row justify-content-md-center">
-              <div className={`${styles.backsplash_container} col-md-6 col-lg-4 text-center`} >
+              <div
+                className={`${styles.backsplash_container} col-md-6 col-lg-4 text-center`}
+              >
                 <div className={`${styles.backsplash} mx-auto`}></div>
-                <Image 
-                  src="/images/phones/device_15_channel_invite.png" 
+                <Image
+                  src="/images/phones/device_15_channel_invite.png"
                   alt="Screenshot of Roads app showing how to select friends to add to a channel."
                   height={400}
                   width={200}
@@ -128,12 +137,15 @@ export default function Home() {
                 />
               </div>
 
-              <div className='col-md-6 col-lg-4 offset-lg-1 my-auto text-center text-md-start'>
+              <div className="col-md-6 col-lg-4 offset-lg-1 my-auto text-center text-md-start">
                 <h3>Control Access</h3>
-                <p>You control who has access to your content through channels. Only invited members can listen and collaborate on your conversations.</p>
+                <p>
+                  You control who has access to your content through channels.
+                  Only invited members can listen and collaborate on your
+                  conversations.
+                </p>
               </div>
             </div>
-
           </div>
         </div>
 
@@ -141,27 +153,27 @@ export default function Home() {
           <h2 className="h2-lg my-lg text-center">Ways To Use Roads</h2>
           <div className="container">
             <div className="row">
-              <UseCase 
+              <UseCase
                 imagePath="/images/icons/note.svg"
                 title="Record voice notes for yourself"
               />
-              <UseCase 
+              <UseCase
                 imagePath="/images/icons/chat.svg"
                 title="Chat with a friend asynchronously"
               />
-              <UseCase 
+              <UseCase
                 imagePath="/images/icons/book.svg"
                 title="Create a virtual book club"
               />
-              <UseCase 
+              <UseCase
                 imagePath="/images/icons/lecture.svg"
                 title="Record a lecture and answer questions"
               />
-              <UseCase 
+              <UseCase
                 imagePath="/images/icons/team.svg"
                 title="Share weekly updates with your team"
               />
-              <UseCase 
+              <UseCase
                 imagePath="/images/icons/podcast.svg"
                 title="Co-create podcasts with others"
               />
@@ -173,56 +185,60 @@ export default function Home() {
           <h2 className="h2-lg my-lg text-center">FAQ&apos;s</h2>
           <div className="row justify-content-center">
             <div className="col-10 col-md-8 col-lg-6">
-              
               <div className="accordion" id="faq_accordion">
-                <FaqItem 
+                <FaqItem
                   itemNum="one"
                   question="How do I start a conversation on Roads?"
                   answer="You simply hit record in the app and start talking. After you finish recording you can add a title and share your recording in a channel so others can listen."
                 />
-                <FaqItem 
+                <FaqItem
                   itemNum="two"
                   question="Who can join my channels?"
                   answer="Channels can be joined by anyone the channel owner chooses. If you are friends with someone on Roads you can add them to a channel automatically, if not you can send them a channel invite link. The channel owner controls who is in the channel."
                 />
-                <FaqItem 
+                <FaqItem
                   itemNum="three"
                   question="How do I listen to comments that have been added to a conversation?"
                   answer="When listening to a conversation if there is a comment, you'll have to option to take the road and listen to the comment. If you decide to listen, when the comment completes playing the original conversation will continue."
                 />
-                <FaqItem 
+                <FaqItem
                   itemNum="four"
                   question="What is the maximum recording length?"
                   answer="Conversations and comments can be any length."
                 />
-                <FaqItem 
+                <FaqItem
                   itemNum="five"
                   question="Can I edit or delete my recordings?"
                   answer="There will be no support for editing audio in the initial version. However, you can always delete a recording that you create."
                 />
-                <FaqItem 
+                <FaqItem
                   itemNum="seven"
                   question="Does the app offer a free trial or a freemium version?"
                   answer="Roads will always have a free version where all the core features will be available. Premium features will also be made available for a small fee."
                 />
-                <FaqItem 
+                <FaqItem
                   itemNum="eight"
                   question="How can I delete my account and data?"
                   answer={
                     <span>
-                      Within the app you can find a <strong>Delete Account</strong> link under settings which will delete your account and all related data. For full instructions <Link href="/delete-account">view this page.</Link>
+                      Within the app you can find a{' '}
+                      <strong>Delete Account</strong> link under settings which
+                      will delete your account and all related data. For full
+                      instructions{' '}
+                      <Link href="/delete-account">view this page.</Link>
                     </span>
                   }
                 />
               </div>
             </div>
           </div>
-          
         </div>
 
         <div id={styles.section_wait_list}>
-          <h2 className='h2-lg my-lg text-center text-primary'>Do More With Your Voice</h2>
-          < AppLinks />
+          <h2 className="h2-lg my-lg text-center text-primary">
+            Do More With Your Voice
+          </h2>
+          <AppLinks />
         </div>
       </main>
     </>
--- src/utils/metaConfig.js
+++ src/utils/metaConfig.js
@@ -1,7 +1,8 @@
 const metaConfig = {
   default: {
-    title: 'Roads Audio - Private Communities, Powered By Voice',
-    description: 'Roads Audio helps you build deeper connections through flexible, on-your-time voice conversations that fit your busy life.',
+    title: 'Vega – AI Money Tracker',
+    description:
+      'Manage your expenses and income effortlessly with AI-powered transaction categorization, smart budgeting, and real-time analytics.',
     image: 'https://roadsaudioassets.s3.amazonaws.com/Roads_Cover.png',
   },
   '/delete-account': {
@@ -11,12 +12,14 @@ const metaConfig = {
   },
   '/blogs/the-evolution-of-audio-from-radio-to-roads': {
     title: 'The Evolution of Audio: From Radio to Roads | Roads Audio App',
-    description: 'Explore the evolution of audio communication, from early radio innovations to the digital age with podcasts, leading up to the transformative Roads app.',
+    description:
+      'Explore the evolution of audio communication, from early radio innovations to the digital age with podcasts, leading up to the transformative Roads app.',
     image: '/images/blogs/001/CommunicationReinvented.png',
   },
   '/blogs/how-to-start-a-private-micro-podcast': {
     title: 'How to Start a Private Micro Podcast With Roads Audio',
-    description: 'Skip the expensive studio setup and get the benefits of podcasting effortlessly. Perfect for businesses, creators, and communities looking for secure, invite-only audio discussions.',
+    description:
+      'Skip the expensive studio setup and get the benefits of podcasting effortlessly. Perfect for businesses, creators, and communities looking for secure, invite-only audio discussions.',
     image: '/images/blogs/002/microPodcasting.png',
   },
 };

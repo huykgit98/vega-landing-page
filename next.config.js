05717cd25153edf8fd6c0ee8a63fd7aa2b1b93ad
Huy Nguyen <huykgit98@gmail.com>
2025-07-04 14:39:17 +0700

    config the rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/functions/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}/functions/v1/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

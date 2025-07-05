d27a6b231ba7cbc2d5e3ab32cbe693f3dd25acf8
Huy Nguyen <huykgit98@gmail.com>
2025-07-05 20:18:04 +0700

    code refactoring

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
   async rewrites() {
     const fn = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/auth-confirm`;
-    return [
-      { source: '/auth/callback', destination: fn },
-      { source: '/reset/callback', destination: fn },
-    ];
+    return {
+      beforeFiles: [
+        { source: '/auth/callback', destination: fn },
+        { source: '/reset/callback', destination: fn },
+      ],
+      fallback: [],
+    };
   },
 };
 module.exports = nextConfig;
